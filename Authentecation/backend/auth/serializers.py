4385eba1f7314b7991d2293be21732421a2ccf95
Heba Allah Hashim <hebaallah.hashim68@gmail.com>
2025-05-12 20:59:43 +0300

    initiated backend and setup models

---
+++ Authentecation/backend/auth/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import CustomUser
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'email', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
